19834efecfaef47352140efa1ce1ed9c579d7266
kataus <kataus@gmail.com>
2022-02-16 19:41:59 +0300

    Примеры к занятию №19 (Hibernate)

---
+++ L10-java8/src/main/java/stream/StudentComparators.java
@@ -0,0 +1,16 @@
+package stream;
+
+import java.util.Comparator;
+
+public class StudentComparators {
+    private StudentComparators() {
+    }
+
+    public static Comparator<Student> getAgeComparator(){
+        return null;
+    }
+
+    public static Comparator<Student> getMarkComparator(){
+        return null;
+    }
+}
